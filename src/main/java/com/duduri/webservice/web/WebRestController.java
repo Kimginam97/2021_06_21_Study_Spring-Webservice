88cdcf005529af0b1a236f545cd07a3283cf63a4
Kimginam97 <rlarlska79@naver.com>
2021-06-21 15:30:09 +0900

    2.SpringBoot & JPA 간단 API 만들기

--- src/main/java/com/duduri/webservice/web/WebRestController.java
+++ src/main/java/com/duduri/webservice/web/WebRestController.java
@@ -1,13 +1,26 @@
 package com.duduri.webservice.web;
 
+import com.duduri.webservice.domain.posts.PostsRepository;
+import com.duduri.webservice.dto.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@AllArgsConstructor
 public class WebRestController {
+
+    private PostsRepository postsRepository;
 
     @GetMapping("/hello")
     public String hello() {
         return "HelloWorld";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto){
+        postsRepository.save(dto.toEntity());
     }
 }
