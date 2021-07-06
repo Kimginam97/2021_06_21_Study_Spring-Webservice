89fc8976a2ad765545c7da6e5c1b34850be3f06f
Kimginam97 <rlarlska79@naver.com>
2021-07-06 14:27:31 +0900

    4장 md 정리하기

--- src/main/java/com/duduri/webservice/service/PostsService.java
+++ src/main/java/com/duduri/webservice/service/PostsService.java
@@ -14,16 +14,18 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 
-@RequiredArgsConstructor
 @Service
+@RequiredArgsConstructor
 public class PostsService {
     private final PostsRepository postsRepository;
 
+    // 글 등록
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
     }
 
+    // 글 수정
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
         Posts posts = postsRepository.findById(id)
@@ -34,6 +36,7 @@ public class PostsService {
         return id;
     }
 
+    // 단건 조회
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
@@ -41,6 +44,7 @@ public class PostsService {
         return new PostsResponseDto(entity);
     }
 
+    // 전체 조회
     @Transactional(readOnly = true)
     public List<PostsListResponseDto> findAllDesc() {
         return postsRepository.findAllDesc().stream()
@@ -48,6 +52,7 @@ public class PostsService {
                 .collect(Collectors.toList());
     }
 
+    // 글 삭제
     @Transactional
     public void delete (Long id) {
         Posts posts = postsRepository.findById(id)
--- src/main/java/com/duduri/webservice/web/IndexController.java
+++ src/main/java/com/duduri/webservice/web/IndexController.java
@@ -8,23 +8,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@RequiredArgsConstructor
 @Controller
+@RequiredArgsConstructor
 public class IndexController {
 
     private final PostsService postsService;
 
+    // 메인
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
+    // 글등록
     @GetMapping("/posts/save")
     public String postsSave(Model model) {
         return "posts-save";
     }
 
+    // 글수정
     @GetMapping("/posts/update/{id}")
     public String postsUpdate(@PathVariable Long id, Model model) {
         PostsResponseDto dto = postsService.findById(id);
